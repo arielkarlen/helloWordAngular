187dcb9a947a0d68cebc4fba0495bbe1c1a2171c
arie.karlen <ariel.karlen@possumus.tech>
2023-05-09 09:28:41 -0300

    Clase 7 | Mapeo de opciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component, OnInit } from '@angular/core';
 interface Tarjeta{
   titulo:string;
   subtitulo:string;
-  nro?:number
+  image:string
+}
+
+interface MenuOptions {
+  texto:string,
+  enlace:string
 }
 
 @Component({
@@ -16,13 +21,21 @@ export class AppComponent implements OnInit{
   
  public title = 'Mi primer APP en Angular';
 
-  public ArregloTarjetas: Tarjeta[]=[]
+ public ArregloTarjetas: Tarjeta[]=[]
+ public MenuList: MenuOptions[]=[]
 
   ngOnInit(): void {
     this.ArregloTarjetas = [
-      {titulo:"Video 1", subtitulo: "Subtitulo"},
-      {titulo:"Video 2", subtitulo: "Subtitulo"},
-      {titulo:"Video 3", subtitulo: "Subtitulo"}
+      {titulo:"Video 1", subtitulo: "Subtitulo video 1", image:"http://images.gofreedownload.net/the-lord-of-the-rings-the-battle-for-middle-earth-ii-addon-30286.jpg"},
+      {titulo:"Video 2", subtitulo: "Subtitulo video 2", image: "http://images.gofreedownload.net/the-lord-of-the-rings-the-battle-for-middle-earth-ii-29163.jpg"},
+      {titulo:"Video 3", subtitulo: "Subtitulo video 3", image:"http://images.gofreedownload.net/the-lord-of-the-rings-conquest-37993.jpg"}
+    ]
+
+    this.MenuList =[
+      {texto: "Opcion 1", enlace:"www.google.com"},
+      {texto: "Opcion 2", enlace:"www.google.com"},
+      {texto: "Opcion 3", enlace:"www.google.com"},
+      {texto: "Opcion 4", enlace:"www.google.com"},
     ]
   }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Output  } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
-  public image:string="https://m.media-amazon.com/images/I/917-4I7qNfL._CR0,325,1735,1735_UX256.jpg"
-  public Titulo:string = "Titulo de la card"
+export class CardComponent  {
+
+
+  // @Input recibe datos, @Output envia datos
+  @Input() dataEntrante:any;
+
+
 }
+
